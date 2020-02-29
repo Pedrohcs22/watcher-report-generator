91ebb735214c92ca2e8d2316326f0e64925c574d
Pedro Henrique Caetano <pedro.hcs22@gmail.com>
2020-02-29 14:55:30 -0300

    Include Junit dependency and tests

--- src/test/java/com/dev/converter/TestFileToEntityConverter.java
+++ src/test/java/com/dev/converter/TestFileToEntityConverter.java
@@ -1,4 +1,66 @@
 package com.dev.converter;
 
+import com.dev.model.Item;
+import com.dev.model.datatransferobject.FileDTO;
+import com.dev.model.enumeration.EntityIdentifier;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests {@link FileToEntityConverter}
+ */
 public class TestFileToEntityConverter {
+    @Test
+    public void testFileToSalesman() {
+        Scanner scanner = new Scanner("001ç1234567891234çPedroç50000");
+        var fileDTO = new FileDTO();
+        FileToEntityConverter.convertFileToEntity(scanner, fileDTO);
+        var salesman = fileDTO.getSalesmen().get(0);
+        assertEquals(salesman.getIdentifier(), EntityIdentifier.SALESMAN);
+        assertEquals(salesman.getDocumentNumber(), "1234567891234");
+        assertEquals(salesman.getName(), "Pedro");
+        assertEquals(salesman.getSalary(), BigDecimal.valueOf(50000));
+    }
+
+    @Test
+    public void testFileToClient() {
+        Scanner scanner = new Scanner("002ç2345675434544345çJose da SilvaçRural");
+        var fileDTO = new FileDTO();
+        FileToEntityConverter.convertFileToEntity(scanner, fileDTO);
+        var client = fileDTO.getClients().get(0);
+        assertEquals(client.getIdentifier(), EntityIdentifier.CLIENT);
+        assertEquals(client.getDocumentNumber(), "2345675434544345");
+        assertEquals(client.getName(), "Jose da Silva");
+        assertEquals(client.getBusinessArea(), "Rural");
+    }
+
+    @Test
+    public void testFileToSale() {
+        Scanner scanner = new Scanner("003ç10ç[1-10-100,2-30-2.50]çPedro");
+        var fileDTO = new FileDTO();
+        FileToEntityConverter.convertFileToEntity(scanner, fileDTO);
+        var sale = fileDTO.getSales().get(0);
+        assertEquals(EntityIdentifier.SALE, sale.getIdentifier());
+        assertEquals(10, sale.getSaleId());
+        assertEquals(2, sale.getItems().size());
+        assertItems(sale.getItems());
+        assertEquals("Pedro", sale.getSalesmanName());
+    }
+
+    private void assertItems(List<Item> itemList) {
+        Item firstItem = itemList.get(0);
+        assertEquals(1, firstItem.getItemId());
+        assertEquals(0, firstItem.getItemPrice().compareTo(BigDecimal.valueOf(100)));
+        assertEquals(10, firstItem.getItemQuantity());
+
+        Item secondItem = itemList.get(1);
+        assertEquals(2, secondItem.getItemId());
+        assertEquals(0, secondItem.getItemPrice().compareTo(BigDecimal.valueOf(2.50)));
+        assertEquals(30, secondItem.getItemQuantity());
+    }
 }
--- src/test/java/com/dev/service/TestCreateReportService.java
+++ src/test/java/com/dev/service/TestCreateReportService.java
@@ -1,4 +1,66 @@
 package com.dev.service;
 
+import com.dev.model.Client;
+import com.dev.model.Item;
+import com.dev.model.Sale;
+import com.dev.model.Salesman;
+import com.dev.model.datatransferobject.FileDTO;
+import com.dev.model.datatransferobject.ReportDTO;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests {@link CreateReportService}
+ */
 public class TestCreateReportService {
+
+    @Test
+    public void testCreateReportDTO() {
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.getSales().addAll(getSales());
+        fileDTO.getSalesmen().addAll(getSalesman());
+        fileDTO.getClients().addAll(getClient());
+
+        ReportDTO reportDTO = CreateReportService.createReportDTO(fileDTO);
+        assertEquals(3, reportDTO.getAmountOfClients());
+        assertEquals(2, reportDTO.getAmountOfSalesman());
+        assertEquals(3, reportDTO.getIdOfMostExpensiveSale());
+        assertEquals("Pedro", reportDTO.getNameOfWorstSalesman());
+    }
+
+    private List<Sale> getSales() {
+        var sale = new Sale();
+        sale.setSaleId(2);
+        sale.setSalesmanName("Pedro");
+        sale.getItems().add(new Item(1, 5, BigDecimal.TEN));
+
+        var sale2 = new Sale();
+        sale2.setSaleId(3);
+        sale2.setSalesmanName("Maria");
+        sale2.getItems().add(new Item(1, 20, BigDecimal.TEN));
+
+        return Arrays.asList(sale, sale2);
+    }
+
+    private List<Salesman> getSalesman() {
+        var salesman = new Salesman();
+        salesman.setName("Pedro");
+        var salesman2 = new Salesman();
+        salesman2.setName("Maria");
+
+        return Arrays.asList(salesman, salesman2);
+    }
+
+    private List<Client> getClient() {
+        var client = new Client();
+        var client2 = new Client();
+        var client3 = new Client();
+
+        return Arrays.asList(client, client2, client3);
+    }
 }
